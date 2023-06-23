b8227a4f1a76eacb6142fd56adefd263f9f6e445
Edelberto Hermann Rosler <edelbertorosler@gmail.com>
2023-06-22 23:37:22 -0300

    Adicionando validação do cnpj no lado do servidor

--- cadastrar_cliente.php
+++ cadastrar_cliente.php
@@ -29,8 +29,8 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
         }
     }
 
-    if ($showAlert) {
-        echo "<script>alert('Já existe um cliente salvo com estes dados.');</script>";
+    if ($showAlert || !validarCNPJ($cnpj)) {
+        echo "<script>alert('O cliente já existe, ou os dados estão incorretos.');</script>";
         echo "<script>window.location.href = 'views/cadastro_cliente.php';</script>";
         exit();
     }
@@ -39,4 +39,60 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
 
     header("Location: index.php");
     exit;
+}
+
+
+function validarCNPJ($cnpj) {
+    $cnpj = preg_replace('/\D/', '', $cnpj);
+
+    // Verifica se todos os dígitos são iguais (situação inválida)
+    if (preg_match('/^(\d)\1+$/', $cnpj)) {
+        return false;
+    }
+
+    // Verifica o tamanho do CNPJ
+    if (strlen($cnpj) !== 14) {
+        return false;
+    }
+
+    // Verifica os dígitos verificadores
+    $tamanho = strlen($cnpj) - 2;
+    $numeros = substr($cnpj, 0, $tamanho);
+    $digitos = substr($cnpj, $tamanho);
+    $soma = 0;
+    $pos = $tamanho - 7;
+    $resultado = 0;
+
+    for ($i = $tamanho; $i >= 1; $i--) {
+        $soma += $numeros[$tamanho - $i] * $pos--;
+        if ($pos < 2) {
+            $pos = 9;
+        }
+    }
+
+    $resultado = $soma % 11 < 2 ? 0 : 11 - ($soma % 11);
+
+    if ($resultado != $digitos[0]) {
+        return false;
+    }
+
+    $tamanho += 1;
+    $numeros = substr($cnpj, 0, $tamanho);
+    $soma = 0;
+    $pos = $tamanho - 7;
+
+    for ($i = $tamanho; $i >= 1; $i--) {
+        $soma += $numeros[$tamanho - $i] * $pos--;
+        if ($pos < 2) {
+            $pos = 9;
+        }
+    }
+
+    $resultado = $soma % 11 < 2 ? 0 : 11 - ($soma % 11);
+
+    if ($resultado != $digitos[1]) {
+        return false;
+    }
+
+    return true;
 }
